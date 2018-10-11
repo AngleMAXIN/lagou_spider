6f0e5559a01f6e58d85f1c1bf4b8542e5fc1b375
AngleMAXIN <1678190746@qq.com>
2018-10-11 20:13:01 +0800

    add spider

--- spider/lagou_spider_util.py
+++ spider/lagou_spider_util.py
@@ -2,6 +2,7 @@
 # -*- coding:utf-8 -*-
 
 import time
+import json
 import random
 import requests
 from lagou_spider.spider import config
@@ -10,7 +11,9 @@ from lxml import etree
 
 class Spider(object):
     __page_num = 30
+
     urls_list = []
+
     __company_list = []
     __jobs_list = []
     __positionId = []
@@ -150,3 +153,42 @@ class Spider(object):
     @property
     def job_requests_list(self):
         return self.__position_result
+
+
+class ZhiLian_Spdier(object):
+    """docstring for Spdier"""
+
+    def __init__(self):
+        self.numfound = 120
+
+    def spider_start_api(self, keyword='', cityId=530, page=0):
+        self.keyword = keyword
+        self.cityId = cityId
+        self.page = page
+
+        for page_number in range(4):
+            # print("===============",self.numfound//60)
+            self.__get_jobs_list(page_number)
+
+    def __get_jobs_list(self, page=0):
+
+        url = "https://fe-api.zhaopin.com/c/i/sou"
+        payload = {'pageSize': 60,
+                   'cityId': self.cityId,  # city id
+                   'education': -1,
+                   'kw': self.keyword,  # keyword
+                   'kt': 3,
+                   'start': 0 + 60 * page}  # next page
+
+        r = requests.get(url, params=payload).text
+        print("the --------- {0} --------".format(page))
+        r_json = json.loads(r)
+        if r_json['code'] == 200:
+            # self.numfound = r_json['data']['numFound']
+            # print("******************",self.numfound)
+            jobs_results_list = r_json['data']['results']
+            for job_info in jobs_results_list:
+                print(job_info['positionURL'], job_info['salary'], job_info['updateDate'],
+                      job_info['workingExp']['name'], job_info['eduLevel']['name'])
+        else:
+            print("zhilian---------拒绝访问了-------------")
